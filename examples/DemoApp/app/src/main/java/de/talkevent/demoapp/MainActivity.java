6001259f130670f4062529adcaa78b52d4cd57fc
Pablo Camino <pablo.camino@ambient-innovation.com>
2016-11-15 16:06:08 +0100

    Add mvn-repo and remove .aar

--- examples/DemoApp/app/src/main/java/de/talkevent/demoapp/MainActivity.java
+++ examples/DemoApp/app/src/main/java/de/talkevent/demoapp/MainActivity.java
@@ -8,15 +8,24 @@ import de.talkevent.android_sdk.Models.TalkeventSDK;
 
 public class MainActivity extends AppCompatActivity {
 
+    TalkeventSDK talkevent;
+    String clientNumber = "";
+    String apiUrl = "";
+    String kurentoRoomUrl = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        talkevent = new TalkeventSDK(this);
     }
 
     public void launchSDK(View src) {
-        TalkeventSDK talkevent = new TalkeventSDK();
-        talkevent.setClientNumber("vcapp");
+
+        talkevent.setClientNumber(clientNumber);
+        talkevent.setApiUrl(apiUrl);
+        talkevent.setKurentoRoomUrl(kurentoRoomUrl);
         talkevent.open();
 
     }
